eaf0d779abccf5c48c4fdb7f3b2e22dbaf59e966
neeld <neeldc2@gmail.com>
2023-12-31 23:40:00 +0530

    Upgraded Spring Boot 2 to Spring Boot 3 (javax is deprecated. uses jakarta) Uses MS-SQL running on Docker instead of running it locally. Attached docker-compose.yml file which can bring up MS-SQL on Docker as a container. Updated README.md to have steps to run the application.

--- src/main/java/com/example/testcasedemo/entity/Person.java
+++ src/main/java/com/example/testcasedemo/entity/Person.java
@@ -1,17 +1,23 @@
 package com.example.testcasedemo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.Data;
-import org.hibernate.annotations.Type;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
-import javax.persistence.*;
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "Person", schema = "Dblock")
 public class Person {
+
     @Id
-    @Type(type = "uuid-char")
+    @JdbcTypeCode(SqlTypes.LONGVARCHAR)
     private UUID id;
 
     @Column(name = "personId")
